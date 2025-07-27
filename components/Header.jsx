a3dc3fc0dcac2763025a389f7602060ba3f22400
Squadsantuy <Squadsantuy272@gmail.com>
2025-07-28 05:20:43 +0700

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <header className="bg-green-500 p-3 flex justify-between items-center text-green-50">
+    <header className="bg-blue-500 p-3 flex justify-between items-center text-blue-50">
       <div>
         <Link href="/" className="text-lg font-bold">
           ISLAMIYAH
