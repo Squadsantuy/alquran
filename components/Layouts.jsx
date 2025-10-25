d2479b914485a21c26d137e67981c259793c9932
Squadsantuy <Squadsantuy272@gmail.com>
2025-10-25 17:19:39 +0700

    Integrate AdSense script in Layouts component
    Added Script component for AdSense integration.

--- components/Layouts.jsx
+++ components/Layouts.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Script from 'next/script' // 1. Impor komponen Script
 import Content from './Content'
 import Header from './Header'
 import Footer from './Footer'
@@ -20,6 +21,15 @@ export default function Layout({ children, name }) {
           crossOrigin=""
         />
       </Head>
+
+      {/* 2. Tambahkan Script AdSense di sini */}
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7183187126446600" // GANTI DENGAN ID ANDA
+        crossOrigin="anonymous"
+        strategy="afterInteractive" // Memuat setelah halaman interaktif
+      />
+
       <main className="text-slate-600">
         <Header />
         <Content>{children}</Content>
